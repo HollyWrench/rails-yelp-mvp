2cf3473af66d6946804cc79c66a7cbe51d147224
Holly Wrench <holly.wrench@gmail.com>
2021-08-18 14:43:10 +0100

    created app, created restuarant model and controller, seeded 12 restaurant instances, added validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# :name :address :phone_number :category
+puts 'Cleaning the database'
+Restaurant.destroy_all
+puts 'Database cleaned!'
+
+puts 'creating restaurants...'
+
+# CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
+12.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: CATEGORIES.sample
+  )
+  puts "Restaurant #{restaurant.name} has been created!"
+end
+
+puts "#{Restaurant.count} have been added! 👌"
